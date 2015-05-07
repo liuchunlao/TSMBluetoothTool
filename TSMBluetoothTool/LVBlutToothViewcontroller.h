cd6ff32d6e01e214221d15a1b18a0c22cdc782cc
liuchunlao <liuchunlao@qq.com>
2015-05-07 17:34:59 +0800

    搭建框架

---
+++ TSMBluetoothTool/LVBlutToothViewcontroller.h
@@ -0,0 +1,17 @@
+//
+//  LVBlutToothViewcontroller.h
+//  TSMBluetoothTool
+//
+//  Created by PBOC CS on 15/5/7.
+//  Copyright (c) 2015年 liuchunlao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface LVBlutToothViewcontroller : UIViewController
+
+
+@property (nonatomic, strong) CBPeripheral *peripheral;
+
+@end
---
+++ TSMBluetoothTool/LVBlutToothViewcontroller.m
@@ -0,0 +1,37 @@
+//
+//  LVBlutToothViewcontroller.m
+//  TSMBluetoothTool
+//
+//  Created by PBOC CS on 15/5/7.
+//  Copyright (c) 2015年 liuchunlao. All rights reserved.
+//
+
+#import "LVBlutToothViewcontroller.h"
+
+@interface LVBlutToothViewcontroller ()
+
+@end
+
+@implementation LVBlutToothViewcontroller
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
